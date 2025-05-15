2d21ec2f4d4a7c2329914301efb67fb379043db4
katsiev1997 <katsiev1997@gmail.com>
2025-05-15 16:51:51 +0300

    create post mutation

---
+++ convex/posts.ts
@@ -0,0 +1,47 @@
+import { v } from "convex/values";
+import { mutation } from "./_generated/server";
+
+export const generateUploadUrl = mutation(async (ctx, args) => {
+	const identity = await ctx.auth.getUserIdentity();
+	if (!identity) throw new Error("Not authenticated");
+	return await ctx.storage.generateUploadUrl();
+});
+
+export const createPost = mutation({
+	args: {
+		caption: v.optional(v.string()),
+		storageId: v.id("_storage"),
+	},
+	handler: async (ctx, args) => {
+		const identity = await ctx.auth.getUserIdentity();
+		if (!identity) throw new Error("Not authenticated");
+
+		const currentUser = await ctx.db
+			.query("users")
+			.withIndex("by_clerk_id", (q) => q.eq("clerkId", identity.subject))
+			.first();
+
+		if (!currentUser) throw new Error("User not found");
+
+		const imageUrl = await ctx.storage.getUrl(args.storageId);
+
+		if (!imageUrl) throw new Error("Image not found");
+		// createPost
+
+		const postId = await ctx.db.insert("posts", {
+			userId: currentUser._id,
+			imageUrl,
+			storageId: args.storageId,
+			caption: args.caption,
+			comments: 0,
+			likes: 0,
+		});
+
+		// increment number of posts for user
+		await ctx.db.patch(currentUser._id, {
+			posts: currentUser.posts + 1,
+		});
+
+		return postId;
+	},
+});

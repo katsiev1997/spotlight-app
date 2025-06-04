db8d9a8710b60875a2e6c8efebdae7913fef1792
katsiev1997 <katsiev1997@gmail.com>
2025-06-04 20:57:50 +0300

    get feed posts query

--- convex/posts.ts
+++ convex/posts.ts
@@ -1,5 +1,6 @@
 import { v } from "convex/values";
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
+import { getAuthUser } from "./users";
 
 export const generateUploadUrl = mutation(async (ctx) => {
 	const identity = await ctx.auth.getUserIdentity();
@@ -13,15 +14,7 @@ export const createPost = mutation({
 		storageId: v.id("_storage"),
 	},
 	handler: async (ctx, args) => {
-		const identity = await ctx.auth.getUserIdentity();
-		if (!identity) throw new Error("Not authenticated");
-
-		const currentUser = await ctx.db
-			.query("users")
-			.withIndex("by_clerk_id", (q) => q.eq("clerkId", identity.subject))
-			.first();
-
-		if (!currentUser) throw new Error("User not found");
+		const currentUser = await getAuthUser(ctx);
 
 		const imageUrl = await ctx.storage.getUrl(args.storageId);
 
@@ -43,5 +36,48 @@ export const createPost = mutation({
 		});
 
 		return postId;
+	},
+});
+
+export const getFeedPosts = query({
+	handler: async (ctx) => {
+		const currentUser = await getAuthUser(ctx);
+
+		const posts = await ctx.db.query("posts").order("desc").collect();
+
+		if (posts.length === 0) return [];
+
+		//enhance posts with user data and interaction data
+		const postsWithInfo = await Promise.all(
+			posts.map(async (post) => {
+				const postAuthor = await ctx.db.get(post.userId);
+
+				const like = await ctx.db
+					.query("likes")
+					.withIndex("by_user_and_post", (q) =>
+						q.eq("userId", currentUser._id).eq("postId", post._id)
+					)
+					.first();
+
+				const bookmark = await ctx.db
+					.query("bookmarks")
+					.withIndex("by_user_and_post", (q) =>
+						q.eq("userId", currentUser._id).eq("postId", post._id)
+					)
+					.first();
+
+				return {
+					...post,
+					author: {
+						_id: postAuthor?._id,
+						username: postAuthor?.username,
+						image: postAuthor?.image,
+					},
+					isLiked: !!like,
+					isBookmarked: !!bookmark,
+				};
+			})
+		);
+		return postsWithInfo;
 	},
 });
